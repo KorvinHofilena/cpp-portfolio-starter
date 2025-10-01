ef07f454d96112cdeb029934a6cccc4e79c07215
Korvin <hofilenakorvin@gmail.com>
2025-10-01 16:32:09 -0700

    Add GitHub Actions CI workflow

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files/portfolio_hello")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files/cpp_portfolio_starter")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
